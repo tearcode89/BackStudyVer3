af77bbddea5c8f43d0d001032962cf7e62ef470a
tearcode89 <sjun8932.3@gmail.com>
2023-08-23 11:44:40 +0900

    2. Node.js 활용 - 함수 실행

---
+++ 0. Note/Section03-NodeJS API 구현/Node JS 활용 - 함수 실행.md
@@ -0,0 +1,4 @@
+브라우저 없이 Javascript를 실행하려면 Node.js가 필요하다.
+
+npm yarn의 존재이유는?
+
---
+++ 1. Class/01-01-token/index.js
@@ -0,0 +1,8 @@
+console.log('안녕하세요');
+
+function getToken(aaaa) {
+    const result = String(Math.floor(Math.random() * aaaa)).padStart(aaaa,'0')
+    console.log(result);
+}
+
+getToken(3)
